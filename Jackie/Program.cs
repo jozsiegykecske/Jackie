4a555a57ed9f54293f2b24810952ee1ccb652440
jozsiegykecske <jozsiegykecske@gmail.com>
2021-09-13 10:51:19 +0200

    Kiindulási állapot

--- Jackie/Program.cs
+++ Jackie/Program.cs
@@ -7,9 +7,45 @@ using System.Threading.Tasks;
 namespace Jackie
 {
   class Program
-  {
+  { 
     static void Main(string[] args)
     {
+      ElsoFeladat();
+      MasodikFeladat();
+      HarmadikFladat();
+      NegyedikFeladat();
+      OtodikFeladat();
+      HatodikFeladat();
+      Console.ReadKey();
+    }
+
+    private static void HatodikFeladat()
+    {
+      throw new NotImplementedException();
+    }
+
+    private static void OtodikFeladat()
+    {
+      
+    }
+
+    private static void NegyedikFeladat()
+    {
+      
+    }
+
+    private static void HarmadikFladat()
+    {
+      
+    }
+
+    private static void MasodikFeladat()
+    {
+      
+    }
+    private static void ElsoFeladat()
+    {
+      throw new NotImplementedException();
     }
   }
 }
